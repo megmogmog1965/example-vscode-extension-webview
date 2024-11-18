b2d7d16863fa6229afbba74524b0b2dbff12a791
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2024-11-18 12:55:59 +0900

    webview から呼ぶ API 定義の型化.

--- src/command/index.ts
+++ src/command/index.ts
@@ -22,12 +22,14 @@ interface IResponse {
 export const callCommand = async (message: ICommand, sendResponse: (res: IResponse) => void)  => {
   const handlers = getHandlers(sendResponse)
 
-  if (!(message.command in handlers)) {
+  // find handler.
+  const command = handlers.find((c) => c.name === message.command)
+  if (!command) {
     throw new Error(`Unknown message command: ${message.command}`)
   }
 
   // call handler.
-  await handlers[message.command](message)
+  await command.handler(message)
 }
 
 /**
@@ -36,15 +38,12 @@ export const callCommand = async (message: ICommand, sendResponse: (res: IRespon
  * @param sendResponse Client JS へレスポンスを返送するためのコールバック関数.
  * @returns handlers by command names.
  */
-function getHandlers(sendResponse: (res: IResponse) => void)
-: { [key: string]: (message: Object) => Promise<void> } {
-  return {
-    findfiles: CommandFactory
+function getHandlers(sendResponse: (res: IResponse) => void): Command[] {
+  return [
+    CommandFactory
+      .command('findfiles')
       .input(z.object({
-        command: z.literal('findfiles'),
-        params: z.object({
-          dir: z.string(),
-        }),
+        dir: z.string(),
       }))
       .query(async (message) => {
         // process.
@@ -57,37 +56,56 @@ function getHandlers(sendResponse: (res: IResponse) => void)
         })
       }),
 
-    createfile: CommandFactory
-      .input(z.object({
-        command: z.literal('createfile'),
-        params: z.object({}),
-      }))
+    CommandFactory
+      .command('createfile')
+      .input(z.object({}))
       .query(async (message) => {
         await createFile(message)
       }),
-  }
+  ]
+}
+
+interface Command {
+  name: string
+  handler: (message: object) => Promise<void>
 }
 
 /**
  * Client JS から `vscode.postMessage()` で呼び出し可能なコマンドのファクトリ.
  */
-class CommandFactory<ZodType extends z.ZodTypeAny> {
+class CommandFactory<ZodType extends z.ZodType<object, z.ZodTypeDef, object>> {
+  _commandName: string
   _schema: ZodType
 
-  constructor(schema: ZodType) {
+  constructor(commandName: string, schema: ZodType) {
+    this._commandName = commandName
     this._schema = schema
   }
 
-  static input<ZodType extends z.ZodTypeAny>(schema: ZodType) {
-    const obj = new CommandFactory(schema)
+  static command(commandName: string) {
+    return new CommandFactory(commandName, z.object({}))
+  }
+
+  input<ParamsType extends z.ZodType<object, z.ZodTypeDef, object>>(paramsSchema: ParamsType) {
+    const schema = z.object({
+      command: z.literal(this._commandName),
+      params: paramsSchema,
+    })
+
+    const obj = new CommandFactory(this._commandName, schema)
     return obj
   }
 
   query(callback: (input: z.infer<typeof this._schema>) => Promise<void>)
-  : (message: Object) => Promise<void> {
-    return async (message: Object) => {
+  : Command {
+    const handler = async (message: object) => {
       const validated = this._schema.parse(message)
       return callback(validated)
+    }
+
+    return {
+      name: this._commandName,
+      handler,
     }
   }
 }
